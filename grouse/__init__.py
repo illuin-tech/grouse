1969e27e5311cc0854634cb1863af492d6a21169
antonioloison <48316195+antonioloison@users.noreply.github.com>
2024-09-24 22:49:25 +0200

    feat: add meta-evaluation to Python package (#7)
    Co-authored-by: António Loison <antonio.loison@student-cs.fr>

--- grouse/__init__.py
+++ grouse/__init__.py
@@ -1,2 +1,3 @@
 from grouse.dtos import EvaluationSample, ExpectedGroundedQAEvaluation
 from grouse.grounded_qa_evaluator import GroundedQAEvaluator
+from grouse.meta_evaluator import meta_evaluate_pipeline
